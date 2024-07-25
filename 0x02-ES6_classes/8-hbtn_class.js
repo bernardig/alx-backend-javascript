aef86bdc089bb3fdf53623cf0f74d20a8dd2ba24
bernardig <bernarddonkor154@gmail.com>
2024-07-25 17:04:49 +0000

    update classes

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,16 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    this._location = value;
+    this._size = size;
+    this._location = location;
   }
 
   [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this.size;
-    }
     if (hint === 'string') {
-      return this.location;
+      return this._location;
     }
-    return this;
+    if (hint === 'number') {
+      return this._size;
+    }
+    return null;
   }
 }
